5394369492932c2c3833b97a70242370f2a5b872
checkiamsiam <issiam02415@gmail.com>
2022-05-04 18:30:06 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.ifdur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+  try{
+    //connection and collection creation
+    await client.connect();
+    const productsCollection = client.db("test").collection("devices");
+
+
+
+  }finally{
+
+  }
+}
+
+
+run().catch(console.log)
 
 
 
