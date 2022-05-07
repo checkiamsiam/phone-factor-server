bc5b4d24a050408513c86b1a3a9e760da90a4bba
checkiamsiam <issiam02415@gmail.com>
2022-05-07 22:11:09 +0600

    put and delete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -27,6 +27,24 @@ async function run() {
       await res.send(result);
     })
 
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateItem = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: req.body
+      };
+      const result = await productsCollection.updateOne(updateItem, updateDoc, options);
+      res.send(result)
+    })
+
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const deleteItem = { _id: ObjectId(id) }; 
+      const result = await productsCollection.deleteOne(deleteItem);
+      res.send(result)
+
+    })
 
 
 
